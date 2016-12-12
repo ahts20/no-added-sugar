01b690acdc12e41f816754e1539b687da8631971
BosenGitHub <jason.liubosen@hotmail.com>
2016-12-12 16:22:59 +0000

    new images

--- workspace_2.1/DungeonOfDoom/src/main/SpriteSheet.java
+++ workspace_2.1/DungeonOfDoom/src/main/SpriteSheet.java
@@ -4,12 +4,14 @@ import java.awt.image.BufferedImage;
 
 public class SpriteSheet {
 	private BufferedImage image;
-	public SpriteSheet(BufferedImage image){
+
+	public SpriteSheet(BufferedImage image) {
 		this.image = image;
 	}
-	public BufferedImage grabImage(int col, int row, int width, int height){
-		BufferedImage img = image.getSubimage((col*240/4), (row*180/3), width, height);
+
+	public BufferedImage grabImage(int col, int row, int width, int height) {
+		BufferedImage img = image.getSubimage((col * 240 / 4), (row * 180 / 3), width, height);
 		return img;
-		
+
 	}
 }
